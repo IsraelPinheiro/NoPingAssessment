4babbae070d8fee7d52b12f1575eeb21c65060f4
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 13:29:01 -0300

    Add Customer data structure

--- app/Http/Resources/Customer.php
+++ app/Http/Resources/Customer.php
@@ -4,16 +4,26 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Customer extends JsonResource
-{
+class Customer extends JsonResource{
     /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
+    public function toArray($request){
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'birthday' => $this->birthday,
+            'last_purchase' => $this->last_purchase,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+        ];
     }
 }
--- database/migrations/2022_01_24_161038_create_customers_table.php
+++ database/migrations/2022_01_24_161038_create_customers_table.php
@@ -4,18 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCustomersTable extends Migration
-{
+class CreateCustomersTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('customers', function (Blueprint $table) {
+    public function up(){
+        Schema::create('customers', function (Blueprint $table){
             $table->id();
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('email')->unique()->nullable()->default(null);
+            $table->string('phone',11)->unique()->nullable()->default(null);
+            $table->date('birthday')->nullable()->default(null);
+            $table->date('last_purchase')->nullable()->default(null);
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +30,7 @@ class CreateCustomersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('customers');
     }
 }
