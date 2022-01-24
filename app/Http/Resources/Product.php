a973e2de8a765686d37cb21e1462571549f38298
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 13:30:12 -0300

    Add Product data Structure

--- app/Http/Resources/Product.php
+++ app/Http/Resources/Product.php
@@ -4,16 +4,27 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Product extends JsonResource
-{
+class Product extends JsonResource{
     /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
+    public function toArray($request){
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'sku' => $this->sku,
+            'description' => $this->description,
+            'in_stock' => $this->in_stock,
+            'supplier_id' => $this->supplier_id,
+            'price' => $this->price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+        ];
     }
 }
--- database/migrations/2022_01_24_161044_create_products_table.php
+++ database/migrations/2022_01_24_161044_create_products_table.php
@@ -4,18 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
-{
+class CreateProductsTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+    public function up(){
+        Schema::create('products', function (Blueprint $table){
+            Schema::create('products', function (Blueprint $table){
+                $table->id();
+                $table->string('name')->unique();
+                $table->string('sku')->unique();
+                $table->text('description')->nullable()->default(null);
+                $table->integer('in_stock')->unsigned()->default(0);
+                $table->bigInteger('supplier_id')->unsigned();
+                $table->decimal('price', 8, 2)->unsigned()->default(0);
+                $table->timestamps();   //created_at and updated_at
+                $table->softDeletes();  //deleted_at
+                $table->bigInteger('created_by')->unsigned();
+                $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
+            });
         });
     }
 
@@ -24,8 +33,7 @@ class CreateProductsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('products');
     }
 }
