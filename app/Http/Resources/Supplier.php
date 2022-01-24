b9dfbf7bba5be25252712a76ed55b034887f8efa
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 14:12:44 -0300

    Add Supplier data structure

--- app/Http/Resources/Supplier.php
+++ app/Http/Resources/Supplier.php
@@ -4,16 +4,23 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Supplier extends JsonResource
-{
+class Supplier extends JsonResource{
     /**
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
+    public function toArray($request){
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+        ];
     }
 }
--- database/migrations/2022_01_24_161056_create_suppliers_table.php
+++ database/migrations/2022_01_24_161056_create_suppliers_table.php
@@ -4,18 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSuppliersTable extends Migration
-{
+class CreateSuppliersTable extends Migration{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up(){
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->timestamps();   //created_at and updated_at
+            $table->softDeletes();  //deleted_at
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable()->default(null);
         });
     }
 
@@ -24,8 +27,7 @@ class CreateSuppliersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down(){
         Schema::dropIfExists('suppliers');
     }
 }
