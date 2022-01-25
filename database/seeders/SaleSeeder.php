8b57c9c9cb80644c39f3c9ffceb25a167826447f
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 22:16:25 -0300

    Add Sales seeding data

--- database/seeders/SaleSeeder.php
+++ database/seeders/SaleSeeder.php
@@ -2,17 +2,38 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sale;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class SaleSeeder extends Seeder
-{
+class SaleSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        DB::table((new Sale())->getTable())->truncate();
+        DB::table('product_sale')->truncate();
+        Sale::create([
+			'customer_id' => '3',
+			'created_at' => '2020-11-12 11:59:00'
+        ]);
+        DB::table('product_sale')->insert([
+            'sale_id' => 1,
+            'product_id' => 4,
+            'sell_price' => 499.00,
+            'sold_amount' => 100,
+            'created_at' => '2020-11-12 11:59:00'
+        ]);
+        DB::table('product_sale')->insert([
+            'sale_id' => 1,
+            'product_id' => 5,
+            'sell_price' => 399.00,
+            'sold_amount' => 100,
+            'created_at' => '2020-11-12 11:59:00'
+        ]);
+
+        $this->command->info('Sales Created');
     }
 }
