d0756892b020081a990e76bcb3a618115e229b88
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 22:16:17 -0300

    AddSuppliers seeding data

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -2,17 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Models\Supplier;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class SupplierSeeder extends Seeder
-{
+class SupplierSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        DB::table((new Supplier())->getTable())->truncate();
+        Supplier::create([
+			'name' => 'Microsoft Corporation',
+			'email' => 'xbox@microsoft.com'
+        ]);
+        Supplier::create([
+			'name' => 'Sony Interactive Entertainment',
+			'email' => 'play@sie.com'
+        ]);
+        Supplier::create([
+			'name' => 'Nintendo Co., Ltd.',
+			'email' => 'games@nintendo.com'
+        ]);
+        Supplier::create([
+			'name' => 'Intel Corporation',
+			'email' => 'info@intel.com'
+        ]);
+        $this->command->info('Suppliers Created');
     }
 }
