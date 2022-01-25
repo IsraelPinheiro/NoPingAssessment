288d0c594f07ff7963d531eefedc62c66745d483
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 22:12:30 -0300

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,15 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder{
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run(){
+        $this->call(SupplierSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(CustomerSeeder::class);
+        $this->call(SaleSeeder::class);
+        if(app()->environment('local')){
+           
+        }
+        if(app()->environment('production')){
+            
+        }
     }
 }
