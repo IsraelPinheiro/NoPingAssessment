5aab549c95dc3c80923a0b8ecbe0b763e3d0dc6e
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-24 22:15:50 -0300

    Add Customers seeding data

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -2,17 +2,53 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class CustomerSeeder extends Seeder
-{
+class CustomerSeeder extends Seeder{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        DB::table((new Customer())->getTable())->truncate();
+        Customer::create([
+			'name' => 'Israel Reis Pinheiro',
+			'email' => 'israel.pinheiro@live.com',
+			'phone' => '85991520250',
+			'birthday' => '1990-08-15',
+			'last_purchase' => null
+        ]);
+        Customer::create([
+			'name' => 'William Henry Gates III',
+			'email' => 'bill.gates@gatesfoundation.com',
+			'phone' => '11904041975',
+			'birthday' => '1955-10-05',
+			'last_purchase' => null
+        ]);
+        Customer::create([
+			'name' => 'Jeffrey Preston Bezos',
+			'email' => 'jeff@amazon.com',
+			'phone' => '92905071994',
+			'birthday' => '1964-01-15',
+			'last_purchase' => null
+        ]);
+        Customer::create([
+			'name' => 'Reginald Fils-Aimé',
+			'email' => 'reggie@nintendo.com',
+			'phone' => '11923091889',
+			'birthday' => '1961-03-25',
+			'last_purchase' => null
+        ]);
+        Customer::create([
+			'name' => 'Phil Spencer',
+			'email' => 'p3@microsoft.com',
+			'phone' => '85915112001',
+			'birthday' => '1968-01-12',
+			'last_purchase' => null
+        ]);
+        $this->command->info('Customers Created');
     }
 }
