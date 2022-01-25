c3102ccb37ce0be617fb08fcb275c3cec66b6b1c
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-25 17:27:07 -0300

    remove created_at from sale product

--- database/seeders/SaleSeeder.php
+++ database/seeders/SaleSeeder.php
@@ -23,15 +23,13 @@ class SaleSeeder extends Seeder{
             'sale_id' => 1,
             'product_id' => 4,
             'sell_price' => 499.00,
-            'sold_amount' => 100,
-            'created_at' => '2020-11-12 11:59:00'
+            'sold_amount' => 100
         ]);
         DB::table('product_sale')->insert([
             'sale_id' => 1,
             'product_id' => 5,
             'sell_price' => 399.00,
-            'sold_amount' => 100,
-            'created_at' => '2020-11-12 11:59:00'
+            'sold_amount' => 100
         ]);
 
         $this->command->info('Sales Created');
