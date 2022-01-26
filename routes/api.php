ea5d0d0cb71a6350a5476d162a563c15bde512fb
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-25 23:20:50 -0300

    Add route for getProducts

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::name('api.')->group(function () {
     //Products
     Route::resource('products', ProductController::class);
     //Sales
+    Route::get('sales/{sale}/products', [SaleController::class,'getProducts']);
     Route::post('sales/{sale}/products', [SaleController::class,'addProduct']);
     Route::delete('sales/{sale}/products', [SaleController::class,'removeProduct']);
     Route::post('sales/{sale}/close', [SaleController::class,'closeSale']);
