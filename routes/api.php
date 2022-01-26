23e6408f822803c6012d2e2e78cc583fdd093a24
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-26 11:49:39 -0300

    Add login route and add sanctum middleware to protect the other routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CustomerController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\SaleController;
@@ -17,21 +18,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::name('api.')->group(function () {
-    //Customers
-    Route::get('customers/{customer}/purchases', [CustomerController::class,'getPurchases']);
-    Route::resource('customers', CustomerController::class);
-    //Products
-    Route::get('products/{product}/supplier', [ProductController::class,'getSupplier']);
-    Route::resource('products', ProductController::class);
-    //Sales
-    Route::get('sales/{sale}/customer', [SaleController::class,'getCustomer']);
-    Route::get('sales/{sale}/products', [SaleController::class,'getProducts']);
-    Route::post('sales/{sale}/products', [SaleController::class,'addProduct']);
-    Route::delete('sales/{sale}/products', [SaleController::class,'removeProduct']);
-    Route::post('sales/{sale}/close', [SaleController::class,'closeSale']);
-    Route::resource('sales', SaleController::class);
-    //Supplisers
-    Route::get('suppliers/{supplier}/products', [SupplierController::class,'getProducts']);
-    Route::resource('suppliers', SupplierController::class);
+Route::name('api.')->group(function(){
+    //Auth
+    Route::post('login', [AuthController::class, 'signin']);
+    Route::middleware('auth:sanctum')->group(function(){
+        //Customers
+        Route::get('customers/{customer}/purchases', [CustomerController::class,'getPurchases']);
+        Route::resource('customers', CustomerController::class);
+        //Products
+        Route::get('products/{product}/supplier', [ProductController::class,'getSupplier']);
+        Route::resource('products', ProductController::class);
+        //Sales
+        Route::get('sales/{sale}/customer', [SaleController::class,'getCustomer']);
+        Route::get('sales/{sale}/products', [SaleController::class,'getProducts']);
+        Route::post('sales/{sale}/products', [SaleController::class,'addProduct']);
+        Route::delete('sales/{sale}/products', [SaleController::class,'removeProduct']);
+        Route::post('sales/{sale}/close', [SaleController::class,'closeSale']);
+        Route::resource('sales', SaleController::class);
+        //Supplisers
+        Route::get('suppliers/{supplier}/products', [SupplierController::class,'getProducts']);
+        Route::resource('suppliers', SupplierController::class);
+    });
 });
