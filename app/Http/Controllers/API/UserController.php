2da17d4cfab741d1402519bc696e29db73b2a2f7
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-26 13:31:29 -0300

    Implement user update

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use App\Http\Resources\User as UserResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends BaseController{
     /**
@@ -45,7 +47,19 @@ class UserController extends BaseController{
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, User $user){
-
+        $validator = Validator::make($request->all(), [
+            'name' => ['sometimes','required','string','min:5'],
+            'email' => ['sometimes','required','email','min:5','unique:users,email,'.$user->id],
+            'password' => ['sometimes','required','string','min:6','confirmed']
+        ]);
+        if($validator->fails()){
+            return $this->sendError('Error validation', $validator->errors());       
+        }
+        $user->name = $request->name ? $request->name : $user->name;
+        $user->email = $request->email ? $request->email : $user->email;
+        $user->password = $request->password ? Hash::make($request->password) : $user->password;
+        $user->save();
+        return $this->sendResponse(new UserResource($user), 'User updated.');
     }
 
     /**
