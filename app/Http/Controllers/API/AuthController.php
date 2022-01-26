bbf74676fbb95089148288f8e78bfa54f2751775
Israel R Pinheiro <israel.pinheiro@live.com>
2022-01-26 11:02:59 -0300

    Add doc for signin and signup and improve signup validator

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,6 +10,12 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends BaseController{
 
+    /**
+     * Try to get a auth token based on the provided credentials.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function signin(Request $request){
         if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
             $user = Auth::user(); 
@@ -22,12 +28,17 @@ class AuthController extends BaseController{
         } 
     }
 
+    /**
+     * Create a new user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function signup(Request $request){
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'confirm_password' => 'required|same:password',
+            'name' => ['required','string','min:5'],
+            'email' => ['required','email','min:5','unique:users,email'],
+            'password' => ['required','string','min:6', 'confirmed']
         ]);
         if($validator->fails()){
             return $this->sendError('Error validation', $validator->errors());       
